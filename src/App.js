f37c9a72b9753e1564e39b55e52663b038b54b96
surajk767 <psurajkumar68@gmail.com>
2024-07-05 14:02:24 +0530

    log page successfully

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+import Login from './Login';
+
+function App() {
+  return (
+    <div >
+      <div className="App">
+      <Login />
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Email: ${email}, Password: ${password}`);
+  };
+
+  return (
+    <div className="container mt-5 ">
+      <div className="row justify-content-center">
+        <div className="col-md-6">
+          <div className="card">
+            <div className="card-header">
+              <h3  >Login</h3>
+            </div>
+            <div className='login-container'>
+              <form onSubmit={handleSubmit}>
+                <div className="form-group mb-3">
+                  <label htmlFor="email">Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group mb-3">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <button type="submit" className="btn btn-primary">
+                  Login
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
